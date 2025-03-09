bed3e9dec5e06fd204ccaf3b8d8f4dfad91386e2
muhammad-ashiqe <146414454+muhammad-ashiqe@users.noreply.github.com>
2025-03-09 13:29:28 +0530

    Update FoodItem.jsx

--- frontend/src/Components/FoodItem/FoodItem.jsx
+++ frontend/src/Components/FoodItem/FoodItem.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { assets } from '../../assets/assets'
-import './FoodItem.css'
+import './foodItem.css'
 import { StoreContext } from '../../Contex/StoreContext';
 import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
